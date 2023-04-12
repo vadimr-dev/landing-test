4c37b6eb79143b503c0bcbf3186fed9e71fddbf3
Vadim <vadim2506200525@gmail.com>
2023-04-12 15:32:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="appWrapper">
-      {/* <Registration /> */}
+      {<Registration />}
       <Rings />
       <BackgroundImage />
       <Header />
